786ef395e6e5ca69bf4cd05a2909c0225f289e3f
dystol <github@dystol.com>
2024-06-08 17:57:47 -0700

    gwagwa

--- char.gd
+++ char.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	$kitty/tail.rotate_x(randf()*40)
+	#$kitty/tail.rotate_x(randf()*40)
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
