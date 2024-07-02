a751708f8661877d9742355e6fc3f0dd6fad5614
rune-scape <spartacrafter@gmail.com>
2024-07-02 12:11:12 -0700

    fixed include guards

--- native/src/register_types.h
+++ native/src/register_types.h
@@ -1,5 +1,4 @@
-#ifndef NECRO_STAR_REGISTER_TYPES_H
-#define NECRO_STAR_REGISTER_TYPES_H
+#pragma once
 
 #include <godot_cpp/core/class_db.hpp>
 
@@ -7,5 +6,3 @@ using namespace godot;
 
 void initialize_new_world_module(ModuleInitializationLevel p_level);
 void uninitialize_new_world_module(ModuleInitializationLevel p_level);
-
-#endif // NECRO_STAR_REGISTER_TYPES_H
